e63f7eb9749e162d563f1b5bad5ae19ca1b988a9
Maxd2026 <maxwell.dolan2026@gbwl.org>
2026-01-29 19:26:11 +0000

    4.3

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+     
+      // ID ARRAY is a block of memory that stores a COLLECTION
+      // of data idems (elements) of the same type
+      // UNDER ONE VARIABLE NAME!!
+
+      // How to DECLARE and CREATE an Array variable
+      String[] apCompSci = new String[13];
+      double[] prices = new double[5]; // default values: 0.0
+      String[] names = new String[5]; // default values: null
+      int[] luckyNums = new int[10]; // default values: 0
+      boolean[] yesNo = new boolean[3]; // default values: false
+
+      // How to ACCESS an item in an Array
+      System.out.println( prices [0] );
+
+
+   } // end main method
+} // end Main CLASS
